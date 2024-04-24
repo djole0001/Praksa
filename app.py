3f26df4f8a186f00fad3472894375a329b34355d
djole0001 <118848665+djole0001@users.noreply.github.com>
2024-04-24 12:37:43 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,7 @@ import replicate
 
 os.environ["REPLICATE_API_TOKEN"] = "r8_FzplpKTEDDkYjR5YBdHd6UkIIi5OqAL1TeXvV"
 
-def with_chatbot(user_input):
-   #return{"role: profesor and location: serbia and city:novi_sad"}
-    # output = replicate.run('a16z-infra/llama13b-v2-chat:df7690f1994d94e96ad9d568eac121aecf50684a0b0963b25a41cc40061269e5', # LLM model
-    #                     input={"prompt": f"{user_input} Assistant: ", # Prompts
-    #                     "temperature":0.1, "top_p":0.9, "max_length":128, "repetition_penalty":1})  
+def with_chatbot(user_input):  
     output = replicate.run(
     "meta/llama-2-70b-chat",
     input={
